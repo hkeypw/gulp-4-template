da8d1331f2601a011e4f975f321dfe872e2a1e65
Art <ak@hkey.ru>
2021-10-12 20:54:55 +0300

    start gulp 4 template configuration

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# gulp-4-template
+# gulp-4 Минимальная сборка для фронтенда
+***
+
+## Используемые пакеты
+***
+
+* [browser-sync](https://browsersync.io/docs/gulp) - автообновление браузера (index: "index.html" - указывает с каким файлом работаем)
+* [gulp-sacc](https://www.npmjs.com/package/gulp-sass) - компиляция SASS,SCSS в CSS;
+* [gulp-autoprefixer](https://github.com/sindresorhus/gulp-autoprefixer) - автоматически расставляет вендорные префиксы в CSS
+* [gulp-clean-css](https://github.com/scniro/gulp-clean-css) - минифицируем css файл (для того чтобы получить не минифицированный необходимо раскомментировать в функции style строку: /* , format: 'beautify' */ 
+* [gulp-concat](https://www.npmjs.com/package/gulp-concat) - конкатенация файлов
+* [gulp-uglify-es](https://www.npmjs.com/package/gulp-uglify-es) - Сжимаем JavaScript
+* [gulp-pug](https://www.npmjs.com/package/gulp-pug) - компиляция Pug в HTML
+* [gulp-sourcemaps](https://www.npmjs.com/package/gulp-sourcemaps) - создание sourcemap для js и css
+* [gulp-plumber](https://www.npmjs.com/package/gulp-plumber) - отслеживание ошибок
+
+## Установка
+***
+> Предполагается, что node js и утилита gulp-cli уже установлены.  
+
+Устанавливаем пакеты из package.json:  
+npm install
+
+## Использование
+***
+
+* Основная команда: gulp запускает слежку за файлами (scss и js) gulp-watch и browserSync и собирает проект в папке dist
+* Сборка проекта: gulp build проект собирается в папке dist
+* gulp styles - компилирует scss в css, расставляет префиксы и минимизирует css в папку dist
+* gulp scripts - объединяет и минифицирует js в папку dist
+* gulp pug2html - компилирует pug в html в папку dist
---
+++ gulpfile.js
@@ -0,0 +1,96 @@
+//  Gulp
+const { src, dest, parallel, series, watch } = require('gulp');
+
+//  Browsersync
+const browserSync = require('browser-sync').create();
+
+//  gulp-sass 
+const sass = require('gulp-sass')(require('sass'))
+	 
+//  Autoprefixer
+const autoprefixer = require('gulp-autoprefixer');
+	 
+//  gulp-clean-css
+const cleancss = require('gulp-clean-css');
+
+//  gulp-concat
+const concat = require('gulp-concat');
+	 
+//  gulp-uglify-es
+const uglify = require('gulp-uglify-es').default;
+
+// gulp-pug
+const pug = require('gulp-pug');
+
+// sourcemap
+const sourcemaps = require('gulp-sourcemaps');
+
+// plumber
+const plumber = require('gulp-plumber');
+
+//  Browsersync
+function browsersync() {
+	browserSync.init({ 
+		server: {
+			baseDir: 'dist/',
+			index: "index.html"
+		}, 
+		notify: false, 
+		online: true 
+	})
+}
+
+// scss to css
+function styles(done) {
+  	src('src/scss/style.scss')
+  	.pipe(plumber())
+  	.pipe(sourcemaps.init())
+  	.pipe(sass().on('error', sass.logError))
+  	.pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true })) 
+  	.pipe(cleancss( { level: { 1: { specialComments: 0 } }/* , format: 'beautify' */ } ))
+  	.pipe(sourcemaps.write('../maps'))
+  	.pipe(dest('dist/assets/css'))
+  	.pipe(browserSync.reload({stream: true})) 
+  	done();
+}
+
+// js
+function scripts() {
+	return src([ 
+		'src/js/app1.js', 
+		'src/js/app2.js', 
+		])
+	.pipe(plumber())
+	.pipe(sourcemaps.init())
+	.pipe(concat('main.min.js')) 
+	.pipe(uglify()) 
+	.pipe(sourcemaps.write('../maps'))
+	.pipe(dest('dist/assets/js/')) 
+	.pipe(browserSync.stream()) 
+}
+
+// pug
+function pug2html(){
+	return src('src/pug/*.pug')
+	.pipe(plumber())
+	.pipe(pug({pretty: true}))
+	.pipe(dest('dist/'))
+}
+
+// task styles() sacc to css
+exports.styles = styles;
+
+// task browsersync()
+exports.browsersync = browsersync;
+
+// task scripts
+exports.scripts = scripts;
+
+// task pug to html
+exports.pug2html = pug2html;
+
+// build 
+exports.build = parallel(pug2html, styles, scripts);
+
+// default
+exports.default = parallel(pug2html, styles, scripts, browsersync);
